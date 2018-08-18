2c2bc8922faf6c73d18eee3b510bea2366eca978
Shannon Pearson <shannon.pearson@goverb.com>
2018-08-18 14:23:50 -0500

    adds error handling

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
+import path from 'path';
 import express from 'express';
+import routes from './routes';
 
 // Express app setup
 const app = express();
+
+// view engine
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'pug');
+
+// use routes
+app.use('/', routes);
+
+// error handlers
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: app.get('env') === 'development' ? err : {},
+  });
+  next();
+});
 
 export default app;
