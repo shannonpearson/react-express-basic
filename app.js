a5470be3793f2b5e5a3217f6482269b4a90bb0b1
Shannon Pearson <shannon.pearson@goverb.com>
2018-08-18 14:07:09 -0500

    refactors to server route

---
+++ app.js
@@ -0,0 +1,6 @@
+import express from 'express';
+
+// Express app setup
+const app = express();
+
+export default app;
--- index.js
+++
@@ -1,8 +0,0 @@
-const logSomething = options => ({
-  ...options,
-  anotherOption: 'hello!',
-});
-
-const options = logSomething({ one: '1', two: '2' });
-
-console.log(options);
