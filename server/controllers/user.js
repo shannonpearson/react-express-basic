1ea811fcc1df343574c1d19dfd1753987c355331
Shannon Pearson <shannon.pearson@goverb.com>
2018-08-20 13:18:31 -0500

    password hashing

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,14 +1,26 @@
-// import User from '../models/user';
+import bcrypt from 'bcrypt';
+
 const { User } = require('../models');
 
 module.exports = {
   create(req, res) {
-    return User
-      .create({
-        username: req.body.username,
-        password: req.body.password,
-      })
-      .then(user => res.status(201).send(user))
-      .catch(error => res.status(400).send(error));
+    const saltRounds = 10;
+    bcrypt.genSalt(saltRounds, (err, salt) => {
+      if (err) {
+        return console.log('error in bcrypt.genSalt');
+      }
+      return bcrypt.hash(req.body.password, salt, (error, hash) => {
+        if (error) {
+          return console.log('error in bcrypt.hash');
+        }
+        return User
+          .create({
+            username: req.body.username,
+            password: hash,
+          })
+          .then(user => res.status(201).send(user))
+          .catch(e => res.status(400).send(e));
+      });
+    });
   },
 };
